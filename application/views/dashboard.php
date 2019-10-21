846bd338cda31874dbac368a29907be7f6cdc7c1
ikbalnurmansyah97 <ikbal240897@gmail.com>
2019-10-22 02:44:34 +0700

    tugas_ci

---
+++ application/views/dashboard.php
@@ -0,0 +1,14 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+?>
+<!DOCTYPE>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>praktikum</h1>
+<p>nama saya adalah ajdsfka</p>
+</body>
+</html>
